a9e9036a244b60085c3ac696ce39be35537483c2
Adrian <147884387+venomiakk@users.noreply.github.com>
2025-05-04 00:34:12 +0200

    user-auth initial project config

---
+++ dealmate-project/auth-server/src/test/java/pl/zzpj/dealmate/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zzpj.dealmate.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
