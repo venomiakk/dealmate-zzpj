7acb5b08b8a9f2109493c2df539f379fd3e75e04
Adrian <147884387+venomiakk@users.noreply.github.com>
2025-05-04 17:34:38 +0200

    added endpoints to api gateway

---
+++ dealmate-project/api-gateway/src/main/java/pl/zzpj/dealmate/apigateway/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package pl.zzpj.dealmate.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        // * : Temporary solution to disable security for all endpoints
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex -> ex.anyExchange().permitAll())
+                .build();
+    }
+}
+
+
+
