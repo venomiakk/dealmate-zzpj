1b589992ae34ca14216e63d571295c13bc055bd3
247780 Sałyga Patryk <247780@edu.p.lodz.pl>
2025-05-05 20:51:39 +0200

    Game Service initial commit

---
+++ dealmate-project/game-service/src/test/java/pl/zzpj/dealmate/gameservice/GameserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zzpj.dealmate.gameservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
