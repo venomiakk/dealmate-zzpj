a224fea367a9187fe93aa80c96a5f8aaa68bcbd0
Adrian <147884387+venomiakk@users.noreply.github.com>
2025-05-25 17:10:31 +0200

    security in game service

---
+++ dealmate-project/game-service/src/main/java/pl/zzpj/dealmate/gameservice/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package pl.zzpj.dealmate.gameservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // *: Endpoint protection example
+        // *: But shouldn't it be in api gateway?
+        http
+                // * Disable CSRF protection for testing purposes
+                // * POST requests are not working with CSRF enabled
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        //.requestMatchers("/user/register").permitAll()
+                        .requestMatchers("/test/authtest").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer(resource -> resource
+                        .jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
+}
